20bd633022f42c52f7fc879c8fe02c25975a77fe
Joel Felipe Ferreira Gomes <joelfelipe07@gmail.com>
2017-03-10 18:19:36 -0300

    Adicionando comentários nos arquivos

--- Trabalho 1.1/Parte 2/arvore.cpp
+++ Trabalho 1.1/Parte 2/arvore.cpp
@@ -7,99 +7,150 @@
 
 using namespace std;
 
+//Verifica se o ppid é pai do processo pid
 bool isPai(long ppid, long pid) {
+
+    //Inicialização das variáveis
     char path[40], line[100], *p;
     FILE* statusf;
 
+    //Procura pelo arquivo status do processo procurado
     snprintf(path, 40, "/proc/%ld/status", pid);
 
+    //Abre o arquivo
     statusf = fopen(path, "r");
+
+    //É verificado se é possivel abrir o arquivo informado
     if(!statusf)
         return false;
 
+    //Leitura do arquivo linha por linha
     while(fgets(line, 100, statusf)) {
+        
+        //Verifica se a linha é a linha que contém o PPid do processo
         if(strncmp(line, "PPid:", 5) != 0)
             continue;
         
+        //É tirado da string a parte 'PPid:' para ser verificado somente o PPid em questão
         p = line + 5;
+
+        //Enquanto houver caracteres nulos, eles são retirados da string;
         while(isspace(*p)) ++p;
 
         break;
     }
 
+    //Fecha o arquivo
     fclose(statusf);
 
+    //Verfica se o PPid passado como parâmetro é igual ao PPid achado no arquivo
     if(ppid == atoi(p))
         return true;
     else
         return false;
 }
 
+//Verifica se o pid é um processo que existe no sistema
 bool isValid(long pid) {
+
+    //Inicialização das variáveis
     char path[40], line[100], *p;
     FILE* statusf;
 
+    //Procura pelo arquivo status do processo procurado
     snprintf(path, 40, "/proc/%ld/status", pid);
 
+    //Abre o arquivo
     statusf = fopen(path, "r");
+
+    //É verificado se é possivel abrir o arquivo informado
     if(!statusf)
         return false;
 
+    //Leitura do arquivo linha por linha
     while(fgets(line, 100, statusf)) {
+
+        //Verifica se a linha é a linha que contém o PPid do processo
         if(strncmp(line, "Pid:", 4) != 0)
             continue;
         
+        //É tirado da string a parte 'Pid:' para ser verificado somente o PPid em questão
         p = line + 4;
+
+        //Enquanto houver caracteres nulos, eles são retirados da string;
         while(isspace(*p)) ++p;
 
         break;
     }
 
+    //Fecha o arquivo
     fclose(statusf);
 
+    //Verfica se o Pid passado como parâmetro é igual ao Pid achado no arquivo
     if(pid == atoi(p))
         return true;
     else
         return false;
 }
 
+//Retorna uma lista com todos os processos abertos no sistema
 list<int> getProcessosAbertos(){
+
+    //Inicialização das variáveis
     FILE *fp;
     char path[1035];
     list<int> processosAbertos;
 
+    //Comando que pega todos pid dos processos abertos
     string comando = "ls /proc | grep '^[0-9]'";
 
+    //O comando é executado no terminal e a saída é armazenada em um arquivo
     fp = popen(comando.c_str(), "r");
+
+    //Verifica se o comando foi executado
     if (fp == NULL) {
         printf("Failed to run command\n" );
         exit(1);
     }
 
-    /* Read the output a line at a time - output it. */
+    //Lê a saída linha por linha
     while (fgets(path, sizeof(path)-1, fp) != NULL) {
+
+        //Adiciona o pid em uma lista de processos abertos
         processosAbertos.push_back(atoi(path));
     }
 
-    /* close */
+    //Fecha o arquivo
     pclose(fp);
 
     return processosAbertos;
 }
 
+//Retorna o JSON da árvore de processos de um determinado Pid
 string getArvore(long pid){
 
+    //Inicializa o JSON com o pid, e setando os filhos
     string json = "{ pid: " + to_string(pid) + ", filhos: [";
 
+    //Inicializa uma lista de processos abertos
     list<int> processosAbertos = getProcessosAbertos();
 
+    //Percorre a lista de processos abertos
     for(list<int>::iterator it = processosAbertos.begin(); it != processosAbertos.end(); ++it){
+
+        //Verifica se o pid é pai do processo atual
         if(isPai(pid,*it)){
+
+            //Verifica se a ultima linha do JSON é '[', para saber se é o primeiro elemento da lista de filhos
             if (json.back() != '[')
                 json += ",";
+
+            //Adiciona ao json a árvore do filho
             json += getArvore(*it);
         }
     }
+
+    //Fecha o JSON
     json += " ] }";
 
     return json;
--- Trabalho 1.1/Parte 2/part2.cpp
+++ Trabalho 1.1/Parte 2/part2.cpp
@@ -13,16 +13,27 @@
 
 using namespace std;
 
-void saveFile(string input, int pid){
+//Salva o JSON em um arquivo nomeado como $pid.txt
+void saveFile(string json, int pid){
+	//Inicializa o nome do arquivo
 	string path = to_string(pid) + ".txt";
+
+	//Inicializa o arquivo
     ofstream out(path.c_str());
-    out << input;
+
+	//Envia o conteudo do json para o arquivo
+    out << json;
+
+    //Fecha o arquivo
     out.close();
+
+    //Retorna uma mensagem de confirmação da geração do arquivo
     printf("Arquivo %s gerado com sucesso\n", path.c_str());
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+
+	//Inicialização das variáveis
 	fd_set readfds;
 	int    num_readable;
 	struct timeval tv;
@@ -33,41 +44,61 @@ int main(int argc, char *argv[])
 	tv.tv_sec = 10;
 	tv.tv_usec = 0;
 
+	//Loop Principal do programa
 	while(1) {
 
+		//Zera as variáveis para o loop
 		fd_stdin = fileno(stdin);
-
 		FD_ZERO(&readfds);
 		FD_SET(fileno(stdin), &readfds);
 		
-		printf("Digite o PID desejado: ");
+		printf("Digite o PID desejado (Digite -1 para parar): ");
 		fflush(stdout);
+
+		//Verifica se o usuário digitou algo
 		num_readable = select(fd_stdin + 1, &readfds, NULL, NULL, &tv);
+
+		//Mensagem de erro na verificação
 		if (num_readable == -1) {
 			fprintf(stderr, "\nError in select : %s\n", strerror(errno));
 			exit(1);
 		}
+
+		//Se o usuário não digitou
 		if (num_readable == 0) {
 			system("clear");
+
+			//Imprime a quantidade de processos ativos
 			printf("Quantidade de processos ativos: ");
 			fflush(stdout);
 			system("ps aux | wc -l");
 			printf("\n");
+
+			//Imprime a quantidade de processos ativos por usuário
 			printf("Quantidade de processos ativos por usuário:\n");
 			fflush(stdout);
 			system("ps -eo user=|sort|uniq -c");
 			printf("\n");
 
+			//Aumenta o tempo da verificação se o usuário digitou algo em 10 segundos
 			tv.tv_sec += 10;
 			
+		//Se o usuário digitou
 		} else {
+			//Leitura da informação digitada pelo usuário
 			num_bytes = read(fd_stdin, buf, MAXBYTES);
 
+			//Mensagem de erro na leitura
 			if (num_bytes < 0) {
 				fprintf(stderr, "\nError on read : %s\n", strerror(errno));
 				exit(1);
 			}
 
+			//Verifica se o usuário digitou -1 (Comando para sair do programa)
+			if(atoi(buf) != -1)
+				break;
+
+			//Verifica se a informação digitada pelo usuário é um processo válido
 			if(isValid(atoi(buf)))
 				saveFile(getArvore(atoi(buf)), atoi(buf));
 			else
